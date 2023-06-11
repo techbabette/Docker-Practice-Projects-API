b92873a24cb0a0ad1b55640a7eecd8adb8e90291
techbabette <marinakrsticrf@gmail.com>
2023-06-11 11:57:02 +0200

    Seperate dev and prod docker compose
    Use
    docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM node:18
 WORKDIR /app
 COPY package.json .
-RUN npm install
+
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+    then npm install; \
+    else npm install --only=production; \
+    fi
+
 COPY . .
 CMD ["npm", "run", "dev"] 
--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get("/projects", (req,res) => {
     res.json(data);
 })
 
+// Writing new entries is possible, though not required
+// app.post("/projects", (req, res) => {
+//     let data = JSON.parse(fs.readFileSync("data/projects.json"));
+//     data.push({"Name" : "Test"});
+//     fs.writeFileSync("data/projects.json", JSON.stringify(data));
+//     res.json(data);
+// })
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
