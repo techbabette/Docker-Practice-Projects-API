d0080b5f4eba51eeb77b98665334ea63f9f3f236
techbabette <marinakrsticrf@gmail.com>
2023-06-14 14:13:55 +0200

    CORS Now possible

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/projects", (req,res) => {
     let data = JSON.parse(fs.readFileSync("data/projects.json"));
